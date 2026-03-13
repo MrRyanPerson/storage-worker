538f387c44b544249946951e3af54b71b801b0b3
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-13 16:50:30 +0000

    Create basic prototype

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/**
+ * Welcome to Cloudflare Workers!
+ *
+ * This is a template for a Scheduled Worker: a Worker that can run on a
+ * configurable interval:
+ * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+import {v4 as uuidv4} from 'uuid';
+
+export default {
+	async fetch(req, env) {
+		const url = new URL(req.url)
+
+		const formData = await request.formData();
+		const files = formData.getAll("images");
+
+		if (!files.length) {
+		return new Response("No files uploaded", { status: 400 });
+		}
+
+		const uploaded = [];
+
+		const uuid = uuidv4();
+
+		for (const file of files) {
+			if (!(file instanceof File)) continue;
+
+			const key = `uploads/${uuid}/${file.name}`;
+
+			await env.STORAGE_BUCKET.put(key, file.stream(), {
+				httpMetadata: {
+				contentType: file.type
+				}
+			});
+
+			uploaded.push(key);
+		}
+
+		return Response.json({
+			success: true,
+			files: uploaded,
+			key: uuid
+		});
+	}
+};
